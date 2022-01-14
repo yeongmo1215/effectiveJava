f726195a5a2339607ab3143edcae6cb9a871da56
정영모 <ym1215.jeong@miracom.co.kr>
2022-01-14 18:14:17 +0900

    아이템 3 - private 생성자나 열거 타입으로 싱글턴임을 보증하라

---
+++ src/main/java/study/effectivejava/item3/Item3Application.java
@@ -0,0 +1,11 @@
+package study.effectivejava.item3;
+
+import study.effectivejava.item2.CalzonePizza;
+import study.effectivejava.item2.NyPizza;
+import study.effectivejava.item2.Pizza;
+
+public class Item3Application {
+    public static void main(String[] args) {
+
+    }
+}
